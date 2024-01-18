852070411f5027311c4b493716cd06e1350a57bb
tinyhoot <78366332+tinyhoot@users.noreply.github.com>
2024-01-18 19:13:16 +0100

    Version bump for prerelease 5.

--- DeathrunRemade/DeathrunInit.cs
+++ DeathrunRemade/DeathrunInit.cs
@@ -25,7 +25,7 @@ namespace DeathrunRemade
     {
         public const string GUID = "com.github.tinyhoot.DeathrunRemade";
         public const string NAME = "Deathrun Remade";
-        public const string VERSION = "0.1.4";
+        public const string VERSION = "0.1.5";
 
         internal static Config _Config;
         internal static ILogHandler _Log;
