9705e78b382e8d954d1b785509a4a982e24bd8aa
tinyhoot <78366332+tinyhoot@users.noreply.github.com>
2024-08-05 10:41:57 +0200

    Fix save data not recognised on gamepass version.

--- DeathrunRemade/Patches/SaveFileMenuPatcher.cs
+++ DeathrunRemade/Patches/SaveFileMenuPatcher.cs
@@ -1,3 +1,6 @@
+using System.Collections;
+using System.Collections.Generic;
+using DeathrunRemade.Handlers;
 using DeathrunRemade.Objects.Attributes;
 using DeathrunRemade.Objects.Enums;
 using HarmonyLib;
@@ -15,27 +18,60 @@ namespace DeathrunRemade.Patches
     internal static class SaveFileMenuPatcher
     {
         private static Color _disabledSaveTint = new Color(0.7f, 0.45f, 0.45f, 0.45f);
+        private static Dictionary<string, bool> _slotSaveData = new();
+
+        /// <summary>
+        /// Check each save game for existing Deathrun save data.
+        /// </summary>
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(SaveLoadManager), nameof(SaveLoadManager.LoadSlotsAsync))]
+        [HarmonyPatch(new [] {typeof(UserStorage), typeof(Dictionary<string, SaveLoadManager.GameInfo>), typeof(IOut<SaveLoadManager.LoadResult>)})]
+        private static IEnumerator CheckForExistingSaveData(IEnumerator passthrough, UserStorage userStorage, Dictionary<string, SaveLoadManager.GameInfo> infoCache)
+        {
+            // Make sure we don't carry over any old, outdated data.
+            _slotSaveData.Clear();
+            yield return passthrough;
+            
+            DeathrunInit._Log.Debug("Checking for existing Deathrun save data.");
+            foreach (string slotName in infoCache.Keys)
+            {
+                var result = new TaskResult<bool>();
+                // This is essentially another load operation like the SaveLoadManager just did. It is possible that this
+                // increases main menu load times substantially on weaker machines with many save games.
+                yield return Hootils.HasExistingSaveData(userStorage, slotName, DeathrunInit.NAME, result);
+                DeathrunInit._Log.Debug($"{slotName}: {result.Get()}");
+                _slotSaveData.Add(slotName, result.Get());
+            }
+        }
         
+        /// <summary>
+        /// Disable the buttons for loading a saved game if we have previously determined they were not made with
+        /// Deathrun.
+        /// </summary>
         [HarmonyPostfix]
         [HarmonyPatch(typeof(MainMenuLoadPanel), nameof(MainMenuLoadPanel.UpdateLoadButtonState))]
         private static void AddDeathrunSaveSlotInfo(MainMenuLoadButton lb)
         {
-            DeathrunInit._Log.Debug($"Looking for existing Deathrun data in slot {lb.saveGame}");
-            if (Hootils.HasExistingSaveData(lb.saveGame, DeathrunInit.NAME.Replace(" ", "")))
+            if (!_slotSaveData.TryGetValue(lb.saveGame, out bool hasSaveData))
             {
-                // lb.saveGameModeText.SetText("Deathrun");
+                DeathrunInit._Log.Warn($"Slot {lb.saveGame} was not in cached slot data!");
+                return;
             }
-            else
-            {
-                DeathrunInit._Log.Debug($"{lb.saveGame} does not have Deathrun save data!");
-                // Make the save file look a little muted in colour.
-                lb.load.GetComponent<Image>().color = _disabledSaveTint;
-                // Clicking it no longer does anything.
-                lb.loadButton.SetActive(false);
-                // Finally, add a tooltip while keeping the deletion dialog tooltip-free.
-                var tooltip = lb.load.EnsureComponent<MenuTooltip>();
-                tooltip.key = "Non-Deathrun saves cannot be loaded while Deathrun is active.";
-            }
+            
+            if (!hasSaveData)
+                DisableSaveSlot(lb, "dr_savefile_invalid");
+        }
+
+        private static void DisableSaveSlot(MainMenuLoadButton slotButton, string tooltip)
+        {
+            // Make the save file look a little muted in colour.
+            slotButton.load.GetComponent<Image>().color = _disabledSaveTint;
+            // Clicking it no longer does anything.
+            slotButton.loadButton.SetActive(false);
+
+            // Finally, add a tooltip while keeping the deletion dialog tooltip-free.
+            var menuTooltip = slotButton.load.EnsureComponent<MenuTooltip>();
+            menuTooltip.key = LocalisationHandler.Get(tooltip);
         }
     }
 }
