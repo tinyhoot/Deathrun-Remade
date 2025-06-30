c2602f3bb78c362bc34b8ce6fc5f7d516aba7d1b
tinyhoot <78366332+tinyhoot@users.noreply.github.com>
2025-06-30 13:48:37 +0200

    #27: Add localisation for deaths and random starts.

--- DeathrunRemade/Handlers/LocalisationHandler.cs
+++ DeathrunRemade/Handlers/LocalisationHandler.cs
@@ -1,3 +1,4 @@
+using System;
 using HootLib.Objects;
 using Nautilus.Handlers;
 
@@ -41,6 +42,44 @@ namespace DeathrunRemade.Handlers
         public static string GetFormatted(string key, params object[] formatArgs)
         {
             return Language.main.GetFormat(key, formatArgs);
+        }
+
+        /// <summary>
+        /// Get a sanitised version of the input string for use as a runtime-generated language key. Primarily useful
+        /// for content that can be edited by users like file names or custom starts.
+        /// </summary>
+        public static string GetSanitisedDynamicKey(string key)
+        {
+            // Turns something like "Bulb Zone" into "bulb_zone".
+            return key.ToLower().Trim().Replace(" ", "_");
+        }
+
+        public static string GetDeathKey(string causeOfDeath)
+        {
+            return "dr_death_cause_" + GetSanitisedDynamicKey(causeOfDeath);
+        }
+
+        public static string GetLocalisedCauseOfDeath(string causeOfDeath)
+        {
+            // Special handling for creatures, which can all bite the player individually.
+            if (Enum.TryParse(causeOfDeath, true, out TechType _))
+            {
+                if (_language.TryGet(causeOfDeath, out string line))
+                    return line;
+            }
+
+            // This should resolve to one of the localised entries in the mod's language files.
+            if (_language.TryGet(GetDeathKey(causeOfDeath), out string line2))
+                return line2;
+
+            // Return the cause directly as a fallback.
+            DeathrunInit._Log.Warn($"Failed to find language key for cause of death '{causeOfDeath}'");
+            return causeOfDeath;
+        }
+
+        public static string GetStartKey(string startLocation)
+        {
+            return "dr_start_" + GetSanitisedDynamicKey(startLocation);
         }
 
         public static void SetTechTypeName(TechType techType, string name)
